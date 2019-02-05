6b7fd129eee1ac20efbd6d7c5221529780695e5d
Ricardo Salcido <rsalcido@liberty.edu>
2019-02-05 16:28:19 -0600

    Week5T

---
+++ KevinGWW5T.js
@@ -0,0 +1,68 @@
+
+var gameSpaces = document.querySelectorAll("td");
+var current_playa = 1;
+
+// delete board
+var buttonClck = document.getElementById("button");
+
+
+buttonClck.onclick = function(e) {
+    for (var i = 0; i < gameSpaces.length; i++) {
+        gameSpaces[i].innerHTML = "";
+
+    }
+};
+
+
+// Handler to handle button clicks
+function clickedSquare(e){
+    console.log("test");
+    if(current_playa === 1 && e.target.innerHTML ===""){
+        e.target.innerHTML = "X";
+        current_playa =2;
+    }
+    else if (e.target.innerHTML ===""){
+        e.target.innerHTML = "O";
+        current_playa =1;
+    }
+
+    //check for winner
+    checkforWinner();
+}
+
+// Add a click event listener for each game space
+for (var i = 0; i < gameSpaces.length; i++) {
+    gameSpaces[i].addEventListener('click', clickedSquare);
+}
+// function to check for a winner
+function checkforWinner(){
+    console.log('Checking for winner');
+
+    if((gameSpaces[0].innerText=== gameSpaces[1].innerText &&gameSpaces[1].innerText === gameSpaces[2].innerText && gameSpaces[2].innerText==="X")
+        ||(gameSpaces[3].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[5].innerText && gameSpaces[5].innerText==="X")
+        ||(gameSpaces[6].innerText=== gameSpaces[7].innerText &&gameSpaces[7].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+        ||(gameSpaces[0].innerText=== gameSpaces[3].innerText &&gameSpaces[3].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="X")
+        ||(gameSpaces[1].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[7].innerText && gameSpaces[7].innerText==="X")
+        ||(gameSpaces[2].innerText=== gameSpaces[5].innerText &&gameSpaces[5].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+        ||(gameSpaces[0].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+        ||(gameSpaces[2].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="X"))
+    {
+        alert("X wins");
+        return false;
+    }
+    else if((gameSpaces[0].innerText=== gameSpaces[1].innerText &&gameSpaces[1].innerText === gameSpaces[2].innerText && gameSpaces[2].innerText==="O")
+        ||(gameSpaces[3].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[5].innerText && gameSpaces[5].innerText==="O")
+        ||(gameSpaces[6].innerText=== gameSpaces[7].innerText &&gameSpaces[7].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+        ||(gameSpaces[0].innerText=== gameSpaces[3].innerText &&gameSpaces[3].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="O")
+        ||(gameSpaces[1].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[7].innerText && gameSpaces[7].innerText==="O")
+        ||(gameSpaces[2].innerText=== gameSpaces[5].innerText &&gameSpaces[5].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+        ||(gameSpaces[0].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+        ||(gameSpaces[2].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="O"))
+    {
+        alert("O wins");
+        return false;
+    }
+
+
+}
+
